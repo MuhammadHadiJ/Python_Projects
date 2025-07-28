6b29559161f48b43cbd33cd4b25f5a407004761b
HadiJiwani <mhahdev2@gmail.com>
2025-07-28 23:57:14 +0500

    ..

--- Harvard CS50 AI/Lecture2/pagerank/pagerank.py
+++ Harvard CS50 AI/Lecture2/pagerank/pagerank.py
@@ -58,6 +58,9 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
+
+    PageOutgoings = corpus.get(page)
+
     raise NotImplementedError
 
 
